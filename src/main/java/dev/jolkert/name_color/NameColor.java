50de3ebb73ddfeb84a39dd7c5e47b7743ea67082
jolkert <jolkertt@gmail.com>
2025-02-02 17:55:05 -0500

    initial repo

---
+++ src/main/java/dev/jolkert/name_color/NameColor.java
@@ -0,0 +1,12 @@
+package dev.jolkert.name_color;
+
+import net.fabricmc.api.ModInitializer;
+
+public class NameColor implements ModInitializer
+{
+
+	@Override
+	public void onInitialize()
+	{
+	}
+}
